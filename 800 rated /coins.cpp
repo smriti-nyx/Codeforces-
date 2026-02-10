4eee8a61724ab4b1b1510b5032a8c518a2ebf513
Seraphine Woods <smriticodedthis@gmail.com>
2026-02-10 23:38:22 +0530

    still working

--- 800 rated /coins.cpp
+++ 800 rated /coins.cpp
@@ -2,53 +2,40 @@
 using namespace std;
 using ll = long long;
 
-int main() {
+int main() {                           // { main
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-     long long t ;
-     cin >> t ;
-     while (t--)
-     {
-        int n , k ;
-        cin >> n >> k ;
-        // assert(k >= 1 && k <= n) && k != 2);
-         // Assert the required constraints
-        assert(k >= 1 && k <= n && n <= 1000000000000000000LL);  // n ≤ 10¹⁸
-        assert(k != 2);                                          // k ≠ 2
 
+    long long t;
+    cin >> t;
 
-         
-         bool condition_met = true;
-         
-         //   2*x + k*y = n
-         for(int i = 0; i <n/2 ; i++){
-             if( 2* i + k  == n);
-             condition_met = true;
-            
-         } 
-         else{
-            condition_met = false;
-         }
-        
-    
-         
-         if(condition_met = true ){
-             cout<<"Yes \n"; } 
-             else {
-             cout<<"No\n";
-         }
-       
-        
+    while (t--) {                      // { while
+        int n, k;
+        cin >> n >> k;
 
-     
+        // constraints
+        assert(k >= 1 && k <= n && n <= 1000000000000000000LL);
+        assert(k != 2);
 
+        bool condition_met = false;
 
+        // 2*x + k*y = n
+        for (int i = 0; i < n; i++) {          // { outer for
+            for (int j = 0; j < n; j++) {      // { inner for
+                if (2 * i + k * j == n || 2* i == n || k* j == n) {
+                    condition_met = true;
+                    break;
+                }                              // } if
+            }                                  // } inner for
+            if (condition_met) break;
+        }                                      // } outer for
 
-     }
-     
+        if (condition_met == true) {           // { if
+            cout << "Yes\n";
+        } else {                               // { else
+            cout << "No\n";
+        }                                      // } if/else
+    }                                          // } while
+
     return 0;
-}
-
-
-
-
+}                                              // } main
