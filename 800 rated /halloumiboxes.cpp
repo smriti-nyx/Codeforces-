e091ae139db6fbe4f1ff52d7f386b6fce23d90a4
Seraphine Woods <smriticodedthis@gmail.com>
2026-02-18 11:16:05 +0530

    early in progress

--- 800 rated /halloumiboxes.cpp
+++ 800 rated /halloumiboxes.cpp
@@ -1,1 +1,57 @@
 #include<bits/stdc++.h>
+using namespace std ;
+using ll = long long ;
+
+
+int main(){
+   ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     long long t ;
+     cin >> t ;
+     while (t--){
+         int n , k ;       //variable input taken 
+         cin>>n>>k;
+
+          vector <int> a(n);      //array input using vector 
+          for(int i = 0 ; i <n; i++){
+              cin>>a[i];
+          }
+
+
+           bool sorted = true;
+           
+           
+           
+          for(int i = 0; i <n-1  ; i ++){
+              if(a[i] > a[i+1]){
+                  sorted = false;
+                  break;
+                }
+              }
+              
+            if(sorted) cout<<"YES\n";
+            else cout<<"NO\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     }
+}
+
