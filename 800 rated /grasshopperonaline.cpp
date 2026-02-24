0deac7dbf8a953dc0e8390a0cc82e1cb6b9011c5
Seraphine Woods <smriticodedthis@gmail.com>
2026-02-24 19:43:10 +0530

    done

--- 800 rated /grasshopperonaline.cpp
+++ 800 rated /grasshopperonaline.cpp
@@ -21,64 +21,29 @@ cin>>x>>k ;
 assert(x >= 1 && x <= 100);
 assert(k >= 2 && k <= 100);
 
-//array defined 
-/*
-vector<int> v;
-for(int i = 0 ; i <= x ; i ++){
-    v.push_back(i);
-      }
+
       
       
       
-      
-      //removing elements divisible by k 
-      v.erase(
-    remove_if(v.begin(), v.end(),
-        [k, x](int value) {
-            return (value % k == 0) && (value != x) && (value != 0);
-        }),
-    v.end()
-);
- int maximum = *max_element(v.begin(), v.end());
- */
-
-
-
-
- 
- if(x<k){
-     cout<<1<<"\n";
-     cout<<x <<"\n";
- }
- 
- 
- 
- else if (x == k){
-     cout<<2<< "\n";
-     cout<<x<< " "<< x<<"\n";
-     
- }
- else{
-     if(x> k && x%k != 0){
-         cout<<1<<"\n";
-         cout<<x<<"\n";
+if(x%k != 0){
+    cout<<1<<"\n";
+    cout<<x<<"\n";
+}
+ else {
+     if ( k ==2){
+         cout<< 2<<"\n";
+         cout<< 3<< " "<< x-3<<"\n";
      }
-     else if ( x>k && k == 2) {
-         int result = x -3;
+     else{
          cout<<2<<"\n";
-         cout<< result<< " "<<3<<"\n";
+         cout<<x -1 <<" "<< 1 <<"\n";
      }
-        else{
-            cout<<2<<"\n";
-            int res = x - 2 ;
-            cout<<2<<"\n";
-            cout<< x - res << "  "<<2<<"\n";
-        }
-       
-
-
  }
+
+
+
  
+
  
     
     
