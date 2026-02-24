6ba0719862ad29e1e5d5c5a372d37cf40fd42fd8
Seraphine Woods <smriticodedthis@gmail.com>
2026-02-25 00:01:19 +0530

    in progress

---
+++ 800 rated /extremelyround.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std ;
+using ll = long long ;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  long long t ;
+  cin>>t;
+  while(t--){
+      
+      
+     //input 
+      int n ;
+      cin>>n ;
+      
+      assert(1≤𝑛≤999999 ) ;
+      
+      //the output 
+      int x ;
+      assert(1≤𝑥≤𝑛);
+      
+      
+      if(x <= 9 ){
+          cout<< x <<"\n";
+          
+      }
+      
+      
+      for(int x = 0 ; x<= n ){
+          cout<< 9 + 
+      }  
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+  }
+  return 0;
+}
+  
