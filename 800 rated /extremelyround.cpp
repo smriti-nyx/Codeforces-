f7633d3c3a5030eeb7b57584b451a8bc0435517d
Seraphine Woods <smriticodedthis@gmail.com>
2026-02-25 23:22:48 +0530

    basic maths

--- 800 rated /extremelyround.cpp
+++ 800 rated /extremelyround.cpp
@@ -1,49 +1,49 @@
 #include<bits/stdc++.h>
-using namespace std ;
-using ll = long long ;
-
+using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  long long t ;
-  cin>>t;
-  while(t--){
-      
-      
-     //input 
-      int n ;
-      cin>>n ;
-      
-      assert(1≤𝑛≤999999 ) ;
-      
-      //the output 
-      int x ;
-      assert(1≤𝑥≤𝑛);
-      
-      
-      if(x <= 9 ){
-          cout<< x <<"\n";
-          
-      }
-      
-      
-      for(int x = 0 ; x<= n ){
-          cout<< 9 + 
-      }  
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-  }
-  return 0;
+    cin.tie(nullptr);
+
+    long long t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        long long power = 1;
+        long long tempnum = n;
+
+        while(tempnum >= 10){
+            tempnum /= 10;
+            power *= 10;
+        }
+
+        vector<long long> v;
+
+        int mult = 0;
+
+        for(long long i = 1; i <= power; i *= 10){
+            for (int j = 1; j <= 9 ; j++){
+                mult = i * j;
+                if(mult > n){
+                    break;
+                }
+                                 v.push_back(mult);
+            }
+           
+        }
+
+        long long result = v.size();
+
+        if(n <= 9){
+            cout << n << "\n";
+        }
+        else{
+            cout << result << "\n";
+        }
+    }
+
+    return 0;
 }
-  
