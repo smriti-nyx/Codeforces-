e937009c1cd7e0745578ceff2ab5c30f8196400e
Smriti Mishra <smriticodedthis@gmail.com>
2026-07-05 20:41:03 +0530

    max min sum

---
+++ 900 rated/vasilije_in_cacak.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+        ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        
+        ll n , k , x;
+        cin>>n>>k>>x;
+        
+         ll min_sum = (k*(k+1))/2;
+        ll max_sum = (k*(2*n-k+1))/2;
+        string result = " ";
+        
+        if(min_sum <= x && x<= max_sum){
+            result = "Yes";
+        }else{
+            result = "No";
+        }
+        
+        cout<<result<<"\n";
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
